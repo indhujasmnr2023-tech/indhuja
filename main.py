3c278da4f87c6bd62f52f401aa9a394375696fb7
indhujasmnr2023-tech <indhujasmnr2023@gmail.com>
2026-02-26 16:17:43 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+
+with st.sidebar:
+    data = option_menu(
+        menu_title="my project",
+        options = [
+            "home",
+            "about",
+            "contact",
+           "services",
+        ]
+    )
